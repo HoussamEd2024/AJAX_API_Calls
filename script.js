bbcba1ed2b875c11c8d49641b0e6ecf15fb5d46b
HoussamEd2024 <houssamedjazouli1@gmail.com>
2025-02-22 21:45:51 +0100

    Promisifying geolocation API using resolve and reject functions in Promise object

--- script.js
+++ script.js
@@ -2,30 +2,6 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
-// Coding Challenge #1
-
-/* 
-In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
-
-Here are your tasks:
-
-PART 1
-1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
-2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}.
-The AJAX call will be done to a URL with this format: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=52.508&longitude=13.381. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
-3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
-4. Chain a .catch method to the end of the promise chain and log errors to the console
-5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
-
-PART 2
-6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
-7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
-
-TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
-TEST COORDINATES 2: 19.037, 72.873
-TEST COORDINATES 2: -33.933, 18.474
-
-*/
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText("beforeend", msg);
@@ -50,10 +26,103 @@ const renderCountry = function (countryData, className = "") {
   countriesContainer.style.opacity = 1;
 };
 
-const whereAmI = function (lat, lng) {
-  fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
-  )
+// Example of Event Loop
+
+// console.log("Task : Start");
+// setTimeout(() => console.log("timer 0 seconds"), 0);
+// Promise.resolve("Resolved promise").then((res) => {
+//   for (let i = 0; i < 10000000000000000000; i++) {}
+//   console.log(res);
+// });
+// console.log("Task : End");
+
+// Create a simple Promise
+/*
+const lotteryPromise = new Promise((resolve, reject) => {
+  console.log("Lotter draw is happening");
+  setTimeout(() => {
+    Math.random() >= 0.5
+      ? resolve("You WIN 🤑")
+      : reject(new Error("You lost your money 😫"));
+  }, 2000);
+});
+
+// Consume the promise created
+
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+*/
+// Promisifying setTimeout
+
+const wait = (sec) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, sec * 1000);
+  });
+};
+
+// wait(2)
+//   .catch(() => {
+//     console.log("I waited for 2 seconds");
+//     return wait(1);
+//   })
+//   .catch(() => console.log("I waited for 1 seconds"));
+
+/*
+wait(1)
+  .then(() => {
+    console.log("1 second passed");
+    return wait(1);
+  })
+  .then(() => {
+    console.log("2 seconds passed");
+    return wait(1);
+  })
+  .then(() => {
+    console.log("3 seconds passed");
+    return wait(1);
+  })
+  .then(() => {
+    console.log("4 seconds passed");
+  });
+
+setTimeout(() => {
+  console.log("1 second passed");
+  setTimeout(() => {
+    console.log("2 seconds passed");
+    setTimeout(() => {
+      console.log("3 seconds passed");
+      setTimeout(() => {
+        console.log("4 seconds passed");
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+*/
+
+const getPosition = () => {
+  return new Promise((resolve, reject) => {
+    // navigator.geolocation.getCurrentPosition(
+    //     (pos) => resolve(pos),
+    //     (err) => reject(err)
+    //   )
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+// getPosition().then((position) => console.log(position));
+
+const whereAmI = function () {
+  getPosition()
+    .then((position) => {
+      console.log(position.coords);
+      let { latitude: lat, longitude: lng } = position.coords;
+      return fetch(
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+      );
+    })
+
     .then((response) => {
       console.log(response);
       if (!response.ok)
@@ -79,6 +148,4 @@ const whereAmI = function (lat, lng) {
     });
 };
 
-whereAmI(52.508, 13.381);
-whereAmI(19.037, 72.873);
-whereAmI(-33.933, 18.474);
+btn.addEventListener("click", whereAmI());
