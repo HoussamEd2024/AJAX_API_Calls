300466c3ac8b49ad39d4a27123078200b87014d7
HoussamEd2024 <houssamedjazouli1@gmail.com>
2025-02-23 10:24:35 +0000

    Version with fetch API and async/await syntax(ES6) and error handling with try-catch syntax

--- script.js
+++ script.js
@@ -27,80 +27,11 @@ const renderCountry = function (countryData, className = "") {
   countriesContainer.style.opacity = 1;
 };
 
-// Example of Event Loop
-
-// console.log("Task : Start");
-// setTimeout(() => console.log("timer 0 seconds"), 0);
-// Promise.resolve("Resolved promise").then((res) => {
-//   for (let i = 0; i < 10000000000000000000; i++) {}
-//   console.log(res);
-// });
-// console.log("Task : End");
-
-// Create a simple Promise
-/*
-const lotteryPromise = new Promise((resolve, reject) => {
-  console.log("Lotter draw is happening");
-  setTimeout(() => {
-    Math.random() >= 0.5
-      ? resolve("You WIN 🤑")
-      : reject(new Error("You lost your money 😫"));
-  }, 2000);
-});
-
-// Consume the promise created
-
-lotteryPromise
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
-*/
-// Promisifying setTimeout
-
 const wait = (sec) => {
   return new Promise((resolve) => {
     setTimeout(resolve, sec * 1000);
   });
 };
-
-// wait(2)
-//   .catch(() => {
-//     console.log("I waited for 2 seconds");
-//     return wait(1);
-//   })
-//   .catch(() => console.log("I waited for 1 seconds"));
-
-/*
-wait(1)
-  .then(() => {
-    console.log("1 second passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("2 seconds passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("3 seconds passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("4 seconds passed");
-  });
-
-setTimeout(() => {
-  console.log("1 second passed");
-  setTimeout(() => {
-    console.log("2 seconds passed");
-    setTimeout(() => {
-      console.log("3 seconds passed");
-      setTimeout(() => {
-        console.log("4 seconds passed");
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
-
-*/
 
 const getPosition = () => {
   return new Promise((resolve, reject) => {
@@ -114,101 +45,141 @@ const getPosition = () => {
 
 // getPosition().then((position) => console.log(position));
 
-// const whereAmI = function () {
-//   getPosition()
-//     .then((position) => {
-//       console.log(position.coords);
-//       let { latitude: lat, longitude: lng } = position.coords;
-//       return fetch(
-//         `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
-//       );
-//     })
+//  btn.addEventListener("click", whereAmI());
 
-//     .then((response) => {
-//       console.log(response);
-//       if (!response.ok)
-//         throw new Error(`Problem with geocoding (${response.status})`);
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       console.log(`You are in ${data.city} ,${data.countryName}`);
-//       return fetch(`https://restcountries.com/v3.1/name/${data.countryName}`);
-//     })
-//     .then((response) => {
-//       if (!response.ok)
-//         throw new Error(`Country Not Found (${response.status})`);
-//       return response.json();
-//     })
-//     .then((data) => {
-//       renderCountry(data[0]);
-//     })
-//     .catch((err) => {
-//       console.error(`${err.message} 💥💥💥`);
-//       renderError(`Something went wrong 💥💥💥 ${err.message}. Try Again!`);
-//     });
-// };
+// Use Async/Await (ES6) replace then
+/*
+const getCountryData = async function (country) {
+  //  fetch(`https://restcountries.com/v3.1/name/${country}`)
+  // .then(res=>console.log(res))
+  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+  const data = await res.json();
+  console.log(data);
+  renderCountry(data[0]);
+};
 
-// btn.addEventListener("click", whereAmI());
-
-///////////////////////////////////////
-// Coding Challenge #2
-
-/* 
-Build the image loading functionality that I just showed you on the screen.
-
-Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
-
-PART 1
-1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
-
-If this part is too tricky for you, just watch the first part of the solution.
-
-PART 2
-2. Comsume the promise using .then and also add an error handler;
-3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
-4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
-5. After the second image has loaded, pause execution for 2 seconds again;
-6. After the 2 seconds have passed, hide the current image.
-
-TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
-
+getCountryData("morocco");
 */
 
-const createImage = (imagePath) => {
-  return new Promise((resolve, reject) => {
-    const image = document.createElement("img");
-    image.src = imagePath;
+const whereAmI = async function () {
+  try {
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
-    image.addEventListener("load", () => {
-      imagesContainer.append(image);
-      resolve(image);
-    });
+    // Reverse Geolocation
+    const resGeo = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+    );
+    if (!resGeo.ok) throw new Error("Problem getting location data");
 
-    image.addEventListener("error", () => {
-      reject(new Error("Image not found"));
-    });
+    const dataGeo = await resGeo.json();
+    console.log(dataGeo);
+    const res = await fetch(
+      `https://restcountries.com/v3.1/name/${dataGeo.countryName}`
+    );
+    if (!res.ok) throw new Error("Problem getting country");
+
+    const data = await res.json();
+    console.log(data);
+
+    renderCountry(data[0]);
+  } catch (err) {
+    console.error(`${err} 💥`);
+    renderCountry(`💥 ${err.message}`);
+  }
+  // Handle errors with catch block
+};
+
+btn.addEventListener("click", whereAmI());
+
+const getJSON = async (url, errorMsg = "Something went wrong") => {
+  const response = await fetch(url);
+  // Throwing errors manually
+  if (!response.ok) throw new Error(`${errorMsg} ${response.status})`);
+  return await response.json();
+};
+
+// Running Promises In sequence
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+//     const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+//     const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+//     console.log([data1.capital, data2.capital, data3.capital]);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// Running Promises In parallel (If one of promises is rejected , the result are rejected)
+// Promise.all
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    //console.log(data);
+    console.log(data.map((d) => d[0].capital));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+get3Countries("usa", "morocco", "egypt");
+
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/portugal`),
+    getJSON(`https://restcountries.com/v3.1/name/algeria`),
+    getJSON(`https://restcountries.com/v3.1/name/germany`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise((_, reject) => {
+    setTimeout(function () {
+      reject(new Error("Request took too long"));
+    }, sec);
   });
 };
 
-let currImage;
+/*
+Promise.race([
+  getJSON(`https://restcountries.com/v3.1/name/morocco`),
+  timeout(2000),
+])
+  .then((res) => console.log(res[0]))
+  .catch((err) => console.error(err));
 
-createImage("img/img-1.jpg")
-  .then((img) => {
-    currImage = img;
-    console.log("Image 1 loaded");
-    return wait(2);
-  })
-  .then(() => {
-    currImage.style.display = "none";
-    return createImage("img/img-2.jpg");
-  })
-  .then((img) => {
-    currImage = img;
-    console.log("Image 2 loaded");
-    return wait(2);
-  })
-  .then(() => {
-    currImage.style.display = "none";
-  })
+// Promise.allSettled (not short circuits return the result of all promises (no matter fulfilled or rejected ))
+
+Promise.allSettled([
+  Promise.resolve("Succes"),
+  Promise.reject("ERROR"),
+  Promise.resolve("Another succes"),
+]).then((res) => console.log(res));
+
+Promise.all([
+  Promise.resolve("Succes"),
+  Promise.reject("ERROR"),
+  Promise.resolve("Another succes"),
+])
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+*/
+
+// Promise.any [ES2021] (return the first fulfilled promise)
+
+Promise.any([
+  Promise.resolve("Succes"),
+  Promise.reject("ERROR"),
+  Promise.resolve("Another succes"),
+])
+  .then((res) => console.log(res))
   .catch((err) => console.error(err));
